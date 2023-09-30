1fb75e0123a521ffcddee5c46135b08c23f32975
Barzi Jaf <barzidev@gmail.com>
2023-09-30 08:51:42 +0200

    added royalty free musics from pixbay

--- js/data/songs.js
+++ js/data/songs.js
@@ -1,36 +1,43 @@
 export const songsList = [{
   'id': '1',
-  'url': '../../music/Eazy.mp3',
-  'time': '05:10',
-  'title': 'Eazy',
-  'artist': 'G-Eazy'
+  'url': '../../music/tvari-tokyo-cafe.mp3',
+  'time': '02:33',
+  'title': 'Tokyo Cafe',
+  'artist': 'TVARI'
 },
 {
   'id': '2',
-  'url': '../../music/Moana.mp3',
-  'time': '02:58',
-  'title': 'Moana',
-  'artist': 'G-Eazy'
+  'url': '../../music/one-last-time.mp3',
+  'time': '01:12',
+  'title': 'One Last Time',
+  'artist': 'ADMfreenocopyrightmusic'
 },
 {
   'id': '3',
-  'url': '../../music/The Plan.mp3',
-  'time': '04:10',
-  'title': 'The Plan',
-  'artist': 'G-Eazy'
+  'url': '../../music/cinematic-adventure-alive.mp3',
+  'time': '04:43',
+  'title': 'Cinematic Adventure | Alive',
+  'artist': 'Alex-Productions'
 },
 {
   'id': '4',
-  'url': '../../music/KILLSHOT.mp3',
-  'time': '04:13',
-  'title': 'KILLSHOT',
-  'artist': 'Eminem'
+  'url': '../../music/i-canx27t-fall-in-love.mp3',
+  'time': '03:07',
+  'title': 'I can\'t fall in love',
+  'artist': 'MerajMelody'
 },
 {
   'id': '5',
-  'url': '../../music/Rap Devil.mp3',
-  'time': '04:43',
-  'title': 'Rap Devil',
-  'artist': 'Machine Gun Kelly'
+  'url': '../../music/the-loft.mp3',
+  'time': '02:44',
+  'title': 'The Loft',
+  'artist': 'Stellarnaut'
+},
+{
+  'id': '6',
+  'url': '../../music/mozart-turkish-march-music.mp3',
+  'time': '00:59',
+  'title': 'Mozart Turkish March Music',
+  'artist': 'JawadAliKhan'
 },
 ]
